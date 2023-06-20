cdf9e695866c81039635ae064843c001e33fb319
Suman <tdkasman2@gmail.com>
2023-06-20 21:39:36 +0545

    small changes

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,10 @@ class Home extends BaseController
 {
     public function index()
     {
+        return view('admin/dashboard');
+    }
+    public function transaction()
+    {
         return view('admin/transaction');
     }
 }
